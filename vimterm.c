7f293d9b81da71ba6d0908927217ad99c4b4df65
mekb the turtle <mekbgithub@icloud.com>
2022-05-12 08:46:40 +1000

    move from char* to #define

--- vimterm.c
+++ vimterm.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-char* terminal = "/usr/bin/kitty";
-char* editor = "/usr/bin/vim";
+#define TERMINAL = "/usr/bin/kitty"
+#define EDITOR   = "/usr/bin/vim"
 int main(int argc, char* argv[]) {
-	char *a[2] = {NULL};
+	char* a[2] = {NULL};
 	char* args[argc+1];
-	args[0] = terminal;
-	args[1] = editor;
+	args[0] = TERMINAL;
+	args[1] = EDITOR;
 	for (int i = 1; i < argc; ++i) {
 		args[i+1] = argv[i];
 	}
