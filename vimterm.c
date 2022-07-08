f1285d07230ca3b086452bce206ec2bdb593acd2
mekb the turtle <mekbgithub@icloud.com>
2022-07-08 15:37:31 +1000

    ???

--- vimterm.c
+++ vimterm.c
@@ -3,7 +3,6 @@
 #define TERMINAL "/usr/bin/kitty"
 #define EDITOR   "/usr/bin/vim"
 int main(int argc, char* argv[]) {
-	char* a[2] = {NULL};
 	char* args[argc+1];
 	args[0] = TERMINAL;
 	args[1] = EDITOR;
